889580e4a923b0135704f080f494877154a54d15
Rohit Kumar <rsbamil66@gamil.com>
2025-03-28 13:22:59 +0530

    Navbar

---
+++ Refocus/src/Components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IoIosReturnRight } from "react-icons/io";
+
+function Button() {
+  return (
+    <div className="min-w-40 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center gap-8">
+      <span className="text-sm font-regular font-medium ">Get Started</span>
+      <IoIosReturnRight />
+    </div>
+  );
+}
+
+export default Button;
---
+++ Refocus/src/Components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "./Button";
+
+function Navbar() {
+  return (
+    <div className="max-w-screen-xl mx-auto p-3 py-4 flex items-center justify-between border-b-[1px] border-zinc-700">
+      <div className="n-left flex items-center">
+        <h1 className="text-2xl  uppercase tracking-wider">Refocus</h1>
+        <div className="links flex gap-14 ml-20">
+          {["Home", "Work", "Culture", "", "News"].map((elem, i) =>
+            elem.length === 0 ? (
+              <span className="w-[2px] h-7 bg-zinc-700"></span>
+            ) : (
+              <a className="text-sm flex items-center gap-2" href="#">
+                {i === 1 ? (
+                  <span
+                    style={{ boxShadow: "0 0 0.45em #00FF19" }}
+                    className="inline-block w-1 h-1  rounded-full bg-green-500"
+                  ></span>
+                ) : null}
+                {elem}
+              </a>
+            )
+          )}
+        </div>
+      </div>
+      <div className="n-right">
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
